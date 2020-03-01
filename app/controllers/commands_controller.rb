5bb7d1f19e497b098cca7e19c815efd25b37f9cc
abeUzi <ibanez1111.music@gmail.com>
2020-03-01 19:27:57 +0900

    [add] routing and controller
    index:トップページ描画
    search:検索処理

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,5 +1,29 @@
 class CommandsController < ApplicationController
   def index
     @commands = Command.all
+    @command_types = CommandType.all
   end
+
+  def search
+    if params[:commandType] != "" then
+      @commands = CommandType.find(params[:commandType]).commands
+    else
+      @commands = Command.all
+    end
+
+    keywords = params[:keyword].split()
+    if keywords.length != 0 then
+        keywords.each_with_index do |w,i|
+          if i == 0 then
+            @commands = @commands.where('command LIKE ? OR description LIKE ?', "%#{w}%","%#{w}%")        
+          else
+            @commands = @commands.where('command LIKE ? OR description LIKE ?', "%#{w}%","%#{w}%")
+          end
+        end
+    end
+    render json:@commands
+  end
+
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'commands#index'
+  get '/commands/search', to: 'commands#search'
 end
